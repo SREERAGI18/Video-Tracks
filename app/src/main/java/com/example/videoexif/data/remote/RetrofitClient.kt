a627170c674bf80edfde21173056aa0867029cd2
sreeraj <sreerajvijayan@gmail.com>
2026-03-31 11:34:31 +0530

    prevent OutOfMemoryException when large file upload request is made

--- app/src/main/java/com/example/videoexif/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/videoexif/data/remote/RetrofitClient.kt
@@ -3,6 +3,7 @@ package com.example.videoexif.data.remote
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -16,13 +17,28 @@ object RetrofitClient {
         coerceInputValues = true
     }
 
+    private val bodyLogger = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val headersLogger = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.HEADERS
+    }
+
     private val okHttpClient = OkHttpClient.Builder()
         .connectTimeout(10, TimeUnit.SECONDS)
         .readTimeout(30, TimeUnit.SECONDS)
         .writeTimeout(30, TimeUnit.SECONDS)
-        .addInterceptor(HttpLoggingInterceptor().apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        })
+        .addInterceptor { chain ->
+            val request = chain.request()
+            // When multipart request is made then large files take too much heap memory throwing OutOfMemoryException.
+            // To prevent this following check.
+            if (request.body is MultipartBody) {
+                headersLogger.intercept(chain)
+            } else {
+                bodyLogger.intercept(chain)
+            }
+        }
         .build()
 
     val videoApiService: VideoApiService by lazy {
